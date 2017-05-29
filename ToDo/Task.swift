920d83838ccca6ea8545112e67fe3991bbc022aa
sebastianasp <sebastian@sjundegatan.se>
2017-05-29 22:52:31 +0200

    Added new VC and a new navigation items

---
+++ ToDo/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  ToDo
+//
+//  Created by Sebastian Asp on 2017-05-29.
+//  Copyright © 2017 Sebastian Asp. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+
+var name = ""
+var important = false
+
+}
---
+++ ToDo/addTaskViewController.swift
@@ -0,0 +1,23 @@
+//
+//  addTaskViewController.swift
+//  ToDo
+//
+//  Created by Sebastian Asp on 2017-05-29.
+//  Copyright © 2017 Sebastian Asp. All rights reserved.
+//
+
+import UIKit
+
+class addTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+
+
+
+}
